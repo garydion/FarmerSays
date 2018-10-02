466e468a93d55991ca3a8b17cc5a5ea5f8941011
garydion <gdion@engineer.com>
2018-10-02 12:06:08 -0500

    Update publish.py

--- publish.py
+++ publish.py
@@ -7,12 +7,10 @@ HOST = "io.adafruit.com"
 PORT = "1883"
 USERNAME = "topher_cantrell"
 PASSWORD = "<ADAFRUIT.IO KEY>"
+TOPIC = "topher_cantrell/feeds/quantum-farmer-chris"
 
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
-
 client.connect(HOST, PORT)
-
 time.sleep(1)
-
-client.publish("topher_cantrell/feeds/quantum-farmer-chris", sys.argv[1])
+client.publish(TOPIC, sys.argv[1])
